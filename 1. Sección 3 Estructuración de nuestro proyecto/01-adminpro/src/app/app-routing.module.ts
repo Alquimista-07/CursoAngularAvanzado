4f3a6d5cc7e22056580f19c7d5d4f0e9a7f55998
Alquimista-07 <jdca.07@gmail.com>
2022-06-29 12:04:44 -0500

    20. Implementando las rutas principales

---
+++ 1. Sección 3 Estructuración de nuestro proyecto/01-adminpro/src/app/app-routing.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'progress',
+    component: ProgressComponent
+  },
+  {
+    path: 'grafica1',
+    component: Grafica1Component
+  },
+  {
+    // Esto me ayuda que si estoy en la ruta con el path vacío me va a 
+    // redireccionar automáticamente al dashboard
+    path: '',
+    redirectTo: '/dashboard', 
+    pathMatch: 'full' 
+  },
+  {
+    path: '**',
+    component: NopagefoundComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }

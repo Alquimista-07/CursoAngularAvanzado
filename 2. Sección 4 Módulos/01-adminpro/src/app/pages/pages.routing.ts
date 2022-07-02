13567a14e3675af5f41025a3f11e09e1105aef56
Alquimista-07 <jdca.07@gmail.com>
2022-07-02 10:29:04 -0500

    35. Rutas hijas - ForChild( )

---
+++ 2. Sección 4 Módulos/01-adminpro/src/app/pages/pages.routing.ts
@@ -0,0 +1,43 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+
+
+const routes: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+          
+          {
+            path: 'dashboard',
+            component: DashboardComponent
+          },
+          {
+            path: 'progress',
+            component: ProgressComponent
+          },
+          {
+            path: 'grafica1',
+            component: Grafica1Component
+          },
+          {
+            // Esto me ayuda que si estoy en la ruta con el path vacío me va a 
+            // redireccionar automáticamente al dashboard
+            path: '',
+            redirectTo: '/dashboard', 
+            pathMatch: 'full' 
+          }
+    
+        ]
+      },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class PagesRoutingModule {}

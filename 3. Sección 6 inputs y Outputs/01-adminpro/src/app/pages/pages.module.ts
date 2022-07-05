2d570cbe7e728132587a71c1e54659a4e136d038
Alquimista-07 <jdca.07@gmail.com>
2022-07-05 10:03:45 -0500

    48. Crear componente incrementador

--- 3. Sección 6 inputs y Outputs/01-adminpro/src/app/pages/pages.module.ts
+++ 3. Sección 6 inputs y Outputs/01-adminpro/src/app/pages/pages.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
+// Modulos
+import { ComponentsModule } from '../components/components.module';
+import { SharedModule } from '../shared/shared.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { ProgressComponent } from './progress/progress.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -19,6 +22,7 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
+    ComponentsModule,
     FormsModule,
     SharedModule,
     RouterModule

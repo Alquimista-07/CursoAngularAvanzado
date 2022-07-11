68088a3aabc554f206c61cf7b48545c7b933b3f9
Alquimista-07 <jdca.07@gmail.com>
2022-07-11 10:53:04 -0500

    Agregado código base para inciar con la sección 7

---
+++ 4. Sección 7 Servicios-temas-rutas-persistencia ajustes/01-adminpro/src/app/pages/pages.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+// Modulos
+import { ComponentsModule } from '../components/components.module';
+import { SharedModule } from '../shared/shared.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { PagesComponent } from './pages.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    Grafica1Component,
+    ProgressComponent,
+    PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    ComponentsModule,
+    FormsModule,
+    SharedModule,
+    RouterModule
+  ],
+  exports: [
+    DashboardComponent,
+    Grafica1Component,
+    ProgressComponent,
+    PagesComponent
+  ]
+})
+export class PagesModule { }

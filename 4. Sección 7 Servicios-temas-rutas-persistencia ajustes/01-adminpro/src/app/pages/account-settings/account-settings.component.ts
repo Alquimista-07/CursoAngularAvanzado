6228db6865732324a22962a36fbb8db58a74d49e
Alquimista-07 <jdca.07@gmail.com>
2022-07-11 14:19:30 -0500

    64. Agregando clases de CSS sin usar ngClass

--- 4. Sección 7 Servicios-temas-rutas-persistencia ajustes/01-adminpro/src/app/pages/account-settings/account-settings.component.ts
+++ 4. Sección 7 Servicios-temas-rutas-persistencia ajustes/01-adminpro/src/app/pages/account-settings/account-settings.component.ts
@@ -9,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class AccountSettingsComponent implements OnInit {
   
   public linkTheme = document.querySelector('#theme');
+  public links: NodeListOf<Element> | undefined;
   
   constructor() { }
 
   ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
   }
 
   changeTheme( theme: string ){
@@ -23,6 +26,28 @@ export class AccountSettingsComponent implements OnInit {
 
     // Guardamos la preferencia del tema en el localstorage
     localStorage.setItem('theme', urlTheme);
+
+    // Marcamos el check en la caja del tema
+    this.checkCurrentTheme();
+
+  }
+
+  // Creamos un metodo para el check
+  checkCurrentTheme() {
+
+    // Barremos y borramos la clase working condicionalmente
+    this.links!.forEach( elemento => {
+
+      elemento.classList.remove('working');
+      const btnTheme = elemento.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href');
+      
+      if( btnThemeUrl === currentTheme ){
+        elemento.classList.add( 'working' );
+      }
+
+    });
 
   }
 

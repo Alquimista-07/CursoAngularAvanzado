66f8b20c573a1f91318c0b24b750a64aa2d0fc4f
Alquimista-07 <jdca.07@gmail.com>
2022-07-11 10:31:24 -0500

    56. Solución a la tarea de Input y Output

--- 3. Sección 6 inputs y Outputs/01-adminpro/src/app/pages/grafica1/grafica1.component.ts
+++ 3. Sección 6 inputs y Outputs/01-adminpro/src/app/pages/grafica1/grafica1.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import { ChartData, Color } from 'chart.js';
-
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
@@ -10,20 +8,10 @@ import { ChartData, Color } from 'chart.js';
 })
 export class Grafica1Component {
 
-   // Definimos los colores para cada una de las entradas
-   public colors: Color[] = [
-    '#6857E6',
-    '#009FEE',
-    '#F02059'
-  ];
-
-  // Doughnut
-  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
-  public doughnutChartData: ChartData<'doughnut'> = {
-    labels: this.doughnutChartLabels,
-    datasets: [
-      { data: [ 350, 450, 100 ], backgroundColor: this.colors }
+   public labels1: string[] = [ 'Pan', 'Jamón', 'Queso' ];
+   
+   public data1: any = [
+      { data: [ 10, 15, 4 ] }
     ]
-  };
 
 }

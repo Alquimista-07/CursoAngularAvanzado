e0520a4ca6696eea0b9ebadbb4e2fcd10274e79d
Alquimista-07 <jdca.07@gmail.com>
2022-07-15 10:09:15 -0500

    68. Uso de Scripts de archivos importados en el index.html en TypeScript

--- 4. Sección 7 Servicios-temas-rutas-persistencia ajustes/01-adminpro/src/app/pages/pages.component.ts
+++ 4. Sección 7 Servicios-temas-rutas-persistencia ajustes/01-adminpro/src/app/pages/pages.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+// Para que typescript no marque error ya que sabemos que la
+// función existe de manera global hacemos lo siguiente:
+declare function customInitFunctions(): void;
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -12,6 +16,8 @@ export class PagesComponent implements OnInit {
   constructor( private settingsService: SettingsService ) { }
 
   ngOnInit(): void {
+    // Llamamos la función global que inicializa todo
+    customInitFunctions();
   }
 
 }

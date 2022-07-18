3e8cb987d7f1341d8b5bf9e53e2e4549bbef466a
Alquimista-07 <jdca.07@gmail.com>
2022-07-18 11:53:05 -0500

    75. Funciones que retornan promesas

--- 5. Sección 8 Observables y Promesas/01-adminpro/src/app/pages/promesas/promesas.component.ts
+++ 5. Sección 8 Observables y Promesas/01-adminpro/src/app/pages/promesas/promesas.component.ts
@@ -12,6 +12,9 @@ export class PromesasComponent implements OnInit {
 
   ngOnInit(): void {
 
+  /*
+  Comentamos el siguiente código ya que en Angular es común usar promesas pero no de la forma que las tenemos
+
     // Creamos una promesa, y en este caso no es asíncrona por defecto ya que se hace de manera secuencial,
     // por lo tanto necesitamos enviar la resolución y esta es la parte asíncrona
     const promesa = new Promise( (resolve, reject) => {
@@ -38,6 +41,24 @@ export class PromesasComponent implements OnInit {
     });
 
     console.log('Fin del Init');
+
+  */
+
+    this.getUsuarios().then( usuarios => {
+      console.log('usuarios: ', usuarios);
+    });
+
+  }
+
+  getUsuarios() {
+
+    return new Promise( resolve =>{
+      
+      fetch('https://reqres.in/api/users')
+        .then( resp => resp.json())
+        .then( body => resolve( body.data ) );
+
+    });
 
   }
 

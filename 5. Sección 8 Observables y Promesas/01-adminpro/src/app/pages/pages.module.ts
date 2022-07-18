7cfffbe1c21773a180eb2862432351ff4040666c
Alquimista-07 <jdca.07@gmail.com>
2022-07-18 10:04:35 -0500

    Agregado código base para inciar con la sección 8

---
+++ 5. Sección 8 Observables y Promesas/01-adminpro/src/app/pages/pages.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+// Modulos
+import { ComponentsModule } from '../components/components.module';
+import { SharedModule } from '../shared/shared.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { PagesComponent } from './pages.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    Grafica1Component,
+    ProgressComponent,
+    PagesComponent,
+    AccountSettingsComponent
+  ],
+  imports: [
+    CommonModule,
+    ComponentsModule,
+    FormsModule,
+    SharedModule,
+    RouterModule
+  ],
+  exports: [
+    AccountSettingsComponent,
+    DashboardComponent,
+    Grafica1Component,
+    ProgressComponent,
+    PagesComponent
+  ]
+})
+export class PagesModule { }

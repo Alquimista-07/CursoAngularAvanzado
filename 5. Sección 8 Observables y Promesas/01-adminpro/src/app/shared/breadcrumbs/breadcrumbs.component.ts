deee94f6bd2866c6d372ac01ba9eb9fc96d96f77
Alquimista-07 <jdca.07@gmail.com>
2022-07-20 16:30:00 -0500

    85. Optimizaciones del Breadcrumbs

--- 5. Sección 8 Observables y Promesas/01-adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ 5. Sección 8 Observables y Promesas/01-adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -8,28 +9,39 @@ import { filter, map } from 'rxjs/operators';
   styles: [
   ]
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
 
   public titulo!: string;
 
+  // NOTA: Agregamos optimización para que cuando se de logaout se destruya la suscripción y no se mantenga
+  //       y gaste recursos innecesarios ya que se pueden crear varias suscripciones
+  public tituloSubs$: Subscription;
+
   constructor( private router: Router ) { 
-    this.getArgumentosRuta();
+
+    this.tituloSubs$ = this.getArgumentosRuta()
+                           .subscribe( ({ titulo }) => {
+                              this.titulo = titulo;
+                              document.title = `AdminPro - ${ titulo }`;
+                           });
+
+  }
+
+  ngOnDestroy(): void {
+    console.log('Llamo onDestoy');
+    this.tituloSubs$.unsubscribe();
   }
 
   // Metodo para traer la información de la data definida en la el routing y el cual nos sirve
   // para asignar el titulo en el Breadcrums
   getArgumentosRuta() {
 
-    this.router.events
+    return this.router.events
     .pipe(
       filter( (event): event is ActivationEnd => event instanceof ActivationEnd ),
       filter( (event: ActivationEnd) => event.snapshot.firstChild === null ),
       map( event => event.snapshot.data )
-    )
-    .subscribe( ({ titulo }) => {
-      this.titulo = titulo;
-      document.title = `AdminPro - ${ titulo }`;
-    });
+    );
 
   }
 

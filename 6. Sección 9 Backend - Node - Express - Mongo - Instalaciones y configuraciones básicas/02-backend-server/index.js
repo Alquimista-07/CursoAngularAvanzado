670d80e10f54dbc507dcb99432c7f2b43397a123
Alquimista-07 <jdca.07@gmail.com>
2022-07-23 10:43:44 -0500

    93. Primera ruta

--- 6. Sección 9 Backend - Node - Express - Mongo - Instalaciones y configuraciones básicas/02-backend-server/index.js
+++ 6. Sección 9 Backend - Node - Express - Mongo - Instalaciones y configuraciones básicas/02-backend-server/index.js
@@ -4,6 +4,16 @@ const express = require('express');
 // Crear el servidor express
 const app = express();
 
+// Rutas
+app.get('/', (req, res) => {
+
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo!!!...'
+    })
+
+});
+
 
 // Para levantar el servidor
 app.listen( 3000, () =>{

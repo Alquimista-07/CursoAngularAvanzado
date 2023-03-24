b89d522c2b48cf1343e15812b99aeceaff1531ae
Alquimista-07 <jdca.07@gmail.com>
2023-03-24 10:24:00 -0500

    131. Búsqueda general en todas las colecciones de forma simultánea

--- 8. Sección 11 Médicos y Hospitales - CRUD - Búsquedas - subida de Imágenes/02-backend-server/controllers/busquedas.js
+++ 8. Sección 11 Médicos y Hospitales - CRUD - Búsquedas - subida de Imágenes/02-backend-server/controllers/busquedas.js
@@ -2,13 +2,30 @@
 
 const { response } = require('express');
 
-const getTodo = (req, res = response) => {
+// Importamos el modelo de usuarios
+const Usuario = require('../models/usuario');
+// Importamos el modelo de medicos
+const Medico = require('../models/medico');
+// Importamos el modelo de hospitales
+const Hospital = require('../models/hospital');
+
+const getTodo = async(req, res = response) => {
 
     const busqueda = req.params.busqueda;
+    const regex = new RegExp( busqueda, 'i' );
+
+    const [ usuarios, medicos, hospitales ] = await Promise.all([
+        Usuario.find({ nombre: regex }),
+        Medico.find({ nombre: regex }),
+        Hospital.find({ nombre: regex })
+    ]);
 
     res.json({
         ok: true,
         busqueda,
+        usuarios,
+        medicos,
+        hospitales,
         msg: 'Ok busqueda'
     })
 

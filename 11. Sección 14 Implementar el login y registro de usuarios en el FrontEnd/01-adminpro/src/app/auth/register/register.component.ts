ed0f515a93a2d26481c1ce981d5da3e7f605dc9c
Alquimista-07 <jdca.07@gmail.com>
2023-04-19 10:41:17 -0500

    163. Formulario de Registro - Reactive Forms

--- 11. Sección 14 Implementar el login y registro de usuarios en el FrontEnd/01-adminpro/src/app/auth/register/register.component.ts
+++ 11. Sección 14 Implementar el login y registro de usuarios en el FrontEnd/01-adminpro/src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
     './register.component.css'
   ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  // Usando formularios reactivos vamos a capturar la información del registro
+  public registerForm = this.fb.group({
+    // Definimos como quiero que luzca el formulario
+    // El primer parametro es un valor por defecto, el segundo parametro son validaciones
+    nombre: ['Pepito', Validators.required],
+    email: ['test100@test.com', Validators.required ],
+    password: ['123456', [ Validators.required, Validators.minLength(5) ]],
+    password2: ['123456', [ Validators.required, Validators.minLength(5) ]],
+    terminos: [ false, Validators.required ]
 
-  ngOnInit(): void {
+  });
+
+  constructor( private fb: FormBuilder ) { }
+
+  // Definimos el método para capturar la información
+  crearUsuario() {
+    console.log(this.registerForm.value);
   }
 
 }

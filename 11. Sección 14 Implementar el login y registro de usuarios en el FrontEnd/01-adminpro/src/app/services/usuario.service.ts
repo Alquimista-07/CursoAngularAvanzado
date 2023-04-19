cfc8609051e06e1379c3520b7e692c61eec48aa4
Alquimista-07 <jdca.07@gmail.com>
2023-04-19 14:34:05 -0500

    170. Guardar información en el LocalStorage

--- 11. Sección 14 Implementar el login y registro de usuarios en el FrontEnd/01-adminpro/src/app/services/usuario.service.ts
+++ 11. Sección 14 Implementar el login y registro de usuarios en el FrontEnd/01-adminpro/src/app/services/usuario.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { LoginForm } from '../interfaces/login-form.interface';
 
+import { tap } from 'rxjs';
+
 import { environment } from 'src/environments/environment';
 
 // Servicio para crear usuarios
@@ -20,13 +22,35 @@ export class UsuarioService {
   crearUsuario( formData: RegisterForm ){
     // Hay que recordar que como el httpClient de Angular trabaja con Observables debemos subscribirnos con el .subscribe()
     // pero en este caso para obviarlo simplemente usamos el return
-    return this.http.post( `${base_url}/usuarios`, formData );
+    // Como estos son observable podemos usar los pipes de rxjs para disparar efectos secundarios que en este caso vamos a disparar llamado tap
+    // para almacenar el token en el localStorage
+    // NOTA: El token también lo podríamos almacenar en el sessionStorage pero cuando cerramos la instancia del navegador este se va a purgar
+    //       y como nuestro token es vigente por 12 horas queremos almacenarlo hasta que caduque y sea renovado
+    return this.http.post( `${base_url}/usuarios`, formData )
+               .pipe(
+                 tap( (resp: any) => {
+                   // Extraemos el token para grabarlo en el localStorage
+                   // La función setItem del localStorage recibe como primer argumento que es como vamos a llamar la llave y como segundo parametro el valor
+                   localStorage.setItem( 'token', resp.token );
+                 })
+                );
   }
 
   loginUsuario( formLoginData: LoginForm ){
     // Hay que recordar que como el httpClient de Angular trabaja con Observables debemos subscribirnos con el .subscribe()
     // pero en este caso para obviarlo simplemente usamos el return
-    return this.http.post( `${base_url}/login`, formLoginData );
+    // Como estos son observable podemos usar los pipes de rxjs para disparar efectos secundarios que en este caso vamos a disparar llamado tap
+    // para almacenar el token en el localStorage
+    // NOTA: El token también lo podríamos almacenar en el sessionStorage pero cuando cerramos la instancia del navegador este se va a purgar
+    //       y como nuestro token es vigente por 12 horas queremos almacenarlo hasta que caduque y sea renovado
+    return this.http.post( `${base_url}/login`, formLoginData )
+               .pipe(
+                tap( (resp: any) => {
+                  // Extraemos el token para grabarlo en el localStorage
+                  // La función setItem del localStorage recibe como primer argumento que es como vamos a llamar la llave y como segundo parametro el valor
+                  localStorage.setItem( 'token', resp.token );
+                })
+               );
   }
 
 }

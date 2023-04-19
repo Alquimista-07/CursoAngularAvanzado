9b4f882d3158744613607180cfc1869ca5a195e7
Alquimista-07 <jdca.07@gmail.com>
2023-04-19 11:53:58 -0500

    165. Validar las contraseñas iguales

--- 11. Sección 14 Implementar el login y registro de usuarios en el FrontEnd/01-adminpro/src/app/auth/register/register.component.ts
+++ 11. Sección 14 Implementar el login y registro de usuarios en el FrontEnd/01-adminpro/src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -18,10 +18,13 @@ export class RegisterComponent {
     // El primer parametro es un valor por defecto, el segundo parametro son validaciones
     nombre: ['Pepito', Validators.required],
     email: ['test100@test.com', [ Validators.required, Validators.email ] ],
-    password: ['', [ Validators.required, Validators.minLength(5) ]],
-    password2: ['', [ Validators.required, Validators.minLength(5) ]],
-    terminos: [ false, Validators.required ]
+    password: ['123456', [ Validators.required, Validators.minLength(5) ]],
+    password2: ['123456', [ Validators.required, Validators.minLength(5) ]],
+    terminos: [ true, Validators.required ]
 
+  }, {
+    // Creamos una validación personalizada para hacer inválido el formulario cuando no se cumpla alguna de las validaciones
+    validators: this.passwordsIguales( 'password', 'password2' )
   });
 
   constructor( private fb: FormBuilder ) { }
@@ -30,6 +33,7 @@ export class RegisterComponent {
   crearUsuario() {
     this.formSubmitted = true;
     console.log(this.registerForm.value);
+    console.log(this.registerForm);
 
     if( this.registerForm.valid ){
       console.log('Posteando formulario');
@@ -44,9 +48,33 @@ export class RegisterComponent {
     return this.registerForm.get(campo)?.invalid && this.formSubmitted ? true : false;
   }
 
+  // Método para validar que las contraseñas sean iguales
+  contrasenasNoValidas() {
+    return this.registerForm.get('password')?.value !== this.registerForm.get('password2')?.value && this.formSubmitted ? true : false;
+  }
+
   // Método para validar el check de términos y condiciones
   aceptarTerminos(){
     return !this.registerForm.get('terminos')?.value && this.formSubmitted;
+  }
+
+  // Validación personalizada para las contraseñas
+  passwordsIguales( pass1Name: string, pass2Name: string ) {
+    
+    return ( formGroup: FormGroup ) => {
+
+      // Extraemos la referencia a los passwords
+      const pass1Control = formGroup.get(pass1Name);
+      const pass2Control = formGroup.get(pass2Name);
+
+      if( pass1Control?.value === pass2Control?.value ){
+        pass2Control?.setErrors(null);
+      }else {
+        pass2Control?.setErrors({ noEsIgual: true })
+      }
+
+    }
+
   }
 
 }

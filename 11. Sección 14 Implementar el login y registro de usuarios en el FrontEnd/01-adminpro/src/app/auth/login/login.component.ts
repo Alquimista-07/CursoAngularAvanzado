6b1e829e7ac2e7233f0cf1d61aa608e9bb6bda10
Alquimista-07 <jdca.07@gmail.com>
2023-04-19 14:23:02 -0500

    169. Login de usuario - normal

--- 11. Sección 14 Implementar el login y registro de usuarios en el FrontEnd/01-adminpro/src/app/auth/login/login.component.ts
+++ 11. Sección 14 Implementar el login y registro de usuarios en el FrontEnd/01-adminpro/src/app/auth/login/login.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +12,31 @@ import { Router } from '@angular/router';
     './login.component.css'
   ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor( private router: Router ) { }
+  public formSubmitted = false;
 
-  ngOnInit(): void {
-  }
+  // Usando formularios reactivos vamos a capturar la información del formulario de login
+  public loginForm = this.fb.group({
+    // Definimos como quiero que luzca el formulario
+    // El primer parametro es un valor por defecto, el segundo parametro son validaciones
+    email: ['test100@test.com', [ Validators.required, Validators.email ] ],
+    password: ['123456', Validators.required ],
+    remember: [ false ]
+
+  });
+
+  constructor( private router: Router, private fb: FormBuilder, private usuarioService: UsuarioService ) { }
 
   login(){
-    this.router.navigateByUrl('/');
+    // this.router.navigateByUrl('/');
+    console.log( this.loginForm.value );
+    this.usuarioService.loginUsuario( this.loginForm.value )
+        .subscribe( resp => {
+          console.log(resp);
+        }, (err) => {
+          Swal.fire('Error', err.error.msg, 'error');
+        });
   }
 
 }
--- 11. Sección 14 Implementar el login y registro de usuarios en el FrontEnd/01-adminpro/src/app/services/usuario.service.ts
+++ 11. Sección 14 Implementar el login y registro de usuarios en el FrontEnd/01-adminpro/src/app/services/usuario.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { RegisterForm } from '../interfaces/register-form.interface';
+import { LoginForm } from '../interfaces/login-form.interface';
 
 import { environment } from 'src/environments/environment';
 
@@ -21,4 +22,11 @@ export class UsuarioService {
     // pero en este caso para obviarlo simplemente usamos el return
     return this.http.post( `${base_url}/usuarios`, formData );
   }
+
+  loginUsuario( formLoginData: LoginForm ){
+    // Hay que recordar que como el httpClient de Angular trabaja con Observables debemos subscribirnos con el .subscribe()
+    // pero en este caso para obviarlo simplemente usamos el return
+    return this.http.post( `${base_url}/login`, formLoginData );
+  }
+
 }

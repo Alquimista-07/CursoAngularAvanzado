4a82790bd6bf08e0f67b7964f7c20cbba99e655c
Alquimista-07 <jdca.07@gmail.com>
2023-04-19 11:07:54 -0500

    164. Validaciones del formulario

--- 11. Sección 14 Implementar el login y registro de usuarios en el FrontEnd/01-adminpro/src/app/auth/register/register.component.ts
+++ 11. Sección 14 Implementar el login y registro de usuarios en el FrontEnd/01-adminpro/src/app/auth/register/register.component.ts
@@ -10,14 +10,16 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
 
+  public formSubmitted = false;
+
   // Usando formularios reactivos vamos a capturar la información del registro
   public registerForm = this.fb.group({
     // Definimos como quiero que luzca el formulario
     // El primer parametro es un valor por defecto, el segundo parametro son validaciones
     nombre: ['Pepito', Validators.required],
-    email: ['test100@test.com', Validators.required ],
-    password: ['123456', [ Validators.required, Validators.minLength(5) ]],
-    password2: ['123456', [ Validators.required, Validators.minLength(5) ]],
+    email: ['test100@test.com', [ Validators.required, Validators.email ] ],
+    password: ['', [ Validators.required, Validators.minLength(5) ]],
+    password2: ['', [ Validators.required, Validators.minLength(5) ]],
     terminos: [ false, Validators.required ]
 
   });
@@ -26,7 +28,25 @@ export class RegisterComponent {
 
   // Definimos el método para capturar la información
   crearUsuario() {
+    this.formSubmitted = true;
     console.log(this.registerForm.value);
+
+    if( this.registerForm.valid ){
+      console.log('Posteando formulario');
+    } else {
+      console.log('Formulario no es correcto!!!...');
+    }
+
+  }
+
+  // Método para validar campos
+  campoNoValido( campo: string ): boolean {
+    return this.registerForm.get(campo)?.invalid && this.formSubmitted ? true : false;
+  }
+
+  // Método para validar el check de términos y condiciones
+  aceptarTerminos(){
+    return !this.registerForm.get('terminos')?.value && this.formSubmitted;
   }
 
 }

60eeac3c5d148e439f3410ac74cfb60c852f9ee7
Alquimista-07 <jdca.07@gmail.com>
2023-07-12 09:37:52 -0500

    200. Paginar los usuarios

--- 13. Sección 16 Mantenimiento de Usuarios y modal de carga de imágenes/01-adminpro/src/app/pages/mantenimientos/usuarios/usuarios.component.ts
+++ 13. Sección 16 Mantenimiento de Usuarios y modal de carga de imágenes/01-adminpro/src/app/pages/mantenimientos/usuarios/usuarios.component.ts
@@ -12,16 +12,42 @@ export class UsuariosComponent implements OnInit {
 
   public totalUsuarios: number = 0;
   public usuarios: Usuario[] = [];
+  // Propiedad para tener la referencia a la página actual
+  public desde: number = 0;
 
   constructor( private usuarioService: UsuarioService ) { }
 
   ngOnInit(): void {
 
-    this.usuarioService.cargarUsuarios( 0 )
+    this.cargarUsuarios();
+
+  }
+
+  // Método para cargar usuarios
+  cargarUsuarios() {
+
+    this.usuarioService.cargarUsuarios( this.desde )
         .subscribe( ({ total, usuarios }) => {
           this.totalUsuarios = total;
           this.usuarios = usuarios;
         });
+
+  }
+
+  // Método para cambiar la página
+  cambiarPagina( valor: number ){
+
+    // Cambiamos el valor actual de la pagina y le sumamos uno
+    this.desde += valor;
+
+    // Validaciones para evitar que el limite se salga de lo estipulado
+    if( this.desde < 0 ){
+      this.desde = 0;
+    } else if ( this.desde >= this.totalUsuarios ){
+      this.desde -= valor;
+    }
+
+    this.cargarUsuarios();
 
   }
 

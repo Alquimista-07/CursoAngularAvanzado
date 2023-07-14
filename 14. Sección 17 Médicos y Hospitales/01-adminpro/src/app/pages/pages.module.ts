1bbe0e71a1a561c295ec183fe4559fe8b1ff5d2d
Alquimista-07 <jdca.07@gmail.com>
2023-07-14 13:37:28 -0500

    Inicio sección 17

---
+++ 14. Sección 17 Médicos y Hospitales/01-adminpro/src/app/pages/pages.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Modulos
+import { ComponentsModule } from '../components/components.module';
+import { SharedModule } from '../shared/shared.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { PagesComponent } from './pages.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    Grafica1Component,
+    ProgressComponent,
+    PagesComponent,
+    AccountSettingsComponent,
+    PromesasComponent,
+    RxjsComponent,
+    PerfilComponent,
+    UsuariosComponent
+  ],
+  imports: [
+    CommonModule,
+    ComponentsModule,
+    FormsModule,
+    SharedModule,
+    RouterModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    AccountSettingsComponent,
+    DashboardComponent,
+    Grafica1Component,
+    ProgressComponent,
+    PagesComponent
+  ]
+})
+export class PagesModule { }
---
+++ 14. Sección 17 Médicos y Hospitales/01-adminpro/src/app/services/busquedas.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Usuario } from '../models/usuario.model';
+
+const base_url = environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusquedasService {
+
+  // NOTA: Lo que tenemos dentro de este servicio lo podríamos usar convirtiendo los guetters en helpers
+  //       para reutilizarlos de manera global
+
+  constructor( private http: HttpClient ) { }
+
+  // Getter para obtener el token
+  get token(): string {
+    return localStorage.getItem('token') || '';
+  }
+
+  // Getter para obtener los headers
+  get headers() {
+    return {
+      headers: {
+        'x-token': this.token
+      }
+    }
+  }
+
+  private transformarUsuarios( resultados: any[] ): Usuario[] {
+    return resultados.map(
+      user => new Usuario( user.nombre, user.email, '', user.img, user.google, user.role, user.uid )
+    );
+  }
+
+  // Servicio centralizado para buscar usuarios, médicos y hospitales
+  bucar( tipo: 'usuarios' | 'medicos' | 'hospitales', termino: string ) {
+    const url = `${base_url}/todo/coleccion/${ tipo }/${ termino }`;
+    return this.http.get<any[]>(url, this.headers )
+               .pipe(
+                map( (resp: any ) => {
+
+                  switch ( tipo ) {
+                    case 'usuarios':
+                      return this.transformarUsuarios( resp.resultados );
+                  
+                    default:
+                      return [];
+                  }
+                })
+               )
+  }
+
+}

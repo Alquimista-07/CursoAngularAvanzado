7ffb014f8406db1497fb5baf5c491cce05037272
Alquimista-07 <jdca.07@gmail.com>
2023-07-20 12:12:25 -0500

    228. Preparar el formulario de creación de médicos

--- 14. Sección 17 Médicos y Hospitales/01-adminpro/src/app/pages/mantenimientos/medicos/medico.component.ts
+++ 14. Sección 17 Médicos y Hospitales/01-adminpro/src/app/pages/mantenimientos/medicos/medico.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Hospital } from 'src/app/models/hospital.model';
+
+import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
   selector: 'app-medico',
@@ -8,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoComponent implements OnInit {
 
-  constructor() { }
+  public medicoForm!: FormGroup;
+  public hospitales: Hospital[] = [];
+
+  constructor( private fb: FormBuilder, private hospitalService: HospitalService ) { }
 
   ngOnInit(): void {
+
+    this.medicoForm = this.fb.group({
+      nombre: ['Hernando', Validators.required],
+      hospital: ['', Validators.required]
+    });
+
+    this.cargarHospitales();
+
+  }
+
+  guardarMedico() {
+    console.log(this.medicoForm.value);
+  }
+
+  cargarHospitales() {
+
+    this.hospitalService.cargarHospitales( 0 )
+        .subscribe( ({ hospitales }) => {
+          this.hospitales = hospitales;
+        });
+
   }
 
 }

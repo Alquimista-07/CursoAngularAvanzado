5d965b2d21773e5058486b337c2349a6e31c5274
Alquimista-07 <jdca.07@gmail.com>
2023-07-25 11:23:26 -0500

    250. LazyLoad

--- 16. Sección 19 Optimizaciones, Lazy Load y despliegues/01-adminpro/src/app/guards/auth.guard.ts
+++ 16. Sección 19 Optimizaciones, Lazy Load y despliegues/01-adminpro/src/app/guards/auth.guard.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 
 import { UsuarioService } from '../services/usuario.service';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor( private usuarioService: UsuarioService, private router: Router ){}
 
@@ -16,6 +16,19 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot){
 
       return this.usuarioService.validarToken()
+                 .pipe(
+                  tap( estaAutenticado => {
+                    if( !estaAutenticado ){
+                      this.router.navigateByUrl('/login');
+                    }
+                  })
+                 );
+
+  }
+
+  canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    
+    return this.usuarioService.validarToken()
                  .pipe(
                   tap( estaAutenticado => {
                     if( !estaAutenticado ){
--- 16. Sección 19 Optimizaciones, Lazy Load y despliegues/01-adminpro/src/app/pages/pages.routing.ts
+++ 16. Sección 19 Optimizaciones, Lazy Load y despliegues/01-adminpro/src/app/pages/pages.routing.ts
@@ -4,22 +4,6 @@ import { NgModule } from '@angular/core';
 import { AuthGuard } from '../guards/auth.guard';
 
 import { PagesComponent } from './pages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProgressComponent } from './progress/progress.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
-import { PromesasComponent } from './promesas/promesas.component';
-import { RxjsComponent } from './rxjs/rxjs.component';
-import { PerfilComponent } from './perfil/perfil.component';
-
-// Mantenimientos
-import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
-import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
-import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
-import { MedicoComponent } from './mantenimientos/medicos/medico.component';
-import { BusquedaComponent } from './busqueda/busqueda.component';
-import { AdminGuard } from '../guards/admin.guard';
-
 
 const routes: Routes = [
     {
@@ -27,107 +11,10 @@ const routes: Routes = [
         component: PagesComponent,
         // Protegemos las rutas
         canActivate: [ AuthGuard ],
-        children: [
-          
-          {
-            path: '',
-            component: DashboardComponent,
-            // Para este tema del breadcrums podemos enviar la data la cual es un objeto que recibe argumentos como por ejemplo el titulo,
-            // y demás información necesaria.
-            data: {
-              titulo: 'Dashboard'
-            }
-          },
-          {
-            path: 'progress',
-            component: ProgressComponent,
-            data: {
-              titulo: 'ProgressBar'
-            }
-          },
-          {
-            path: 'grafica1',
-            component: Grafica1Component,
-            data: {
-              titulo: 'Grafica #1'
-            }
-          },
-          {
-            path: 'account-settings',
-            component: AccountSettingsComponent,
-            data: {
-              titulo: 'Ajustes de cuenta'
-            }
-          },
-          {
-            path: 'buscar/:termino',
-            component: BusquedaComponent,
-            data: {
-              titulo: 'Busquedas'
-            }
-          },
-          {
-            path: 'promesas',
-            component: PromesasComponent,
-            data: {
-              titulo: 'Promesas'
-            }
-          },
-          {
-            path: 'rxjs',
-            component: RxjsComponent,
-            data: {
-              titulo: 'Rxjs'
-            }
-          },
-          {
-            path: 'perfil',
-            component: PerfilComponent,
-            data: {
-              titulo: 'Perfil de usuario'
-            }
-          },
-
-          // Mantenimientos
-          // Usuarios
-          // Ruts de administrador, a la cual le agregamos el guard
-          {
-            path: 'usuarios',
-            canActivate: [ AdminGuard ],
-            component: UsuariosComponent,
-            data: {
-              titulo: 'Mantenimiento de Usuarios'
-            }
-          },
-
-          // Hospitales
-          {
-            path: 'hospitales',
-            component: HospitalesComponent,
-            data: {
-              titulo: 'Mantenimiento de Hospitales'
-            }
-          },
-
-          // Médicos
-          {
-            path: 'medicos',
-            component: MedicosComponent,
-            data: {
-              titulo: 'Mantenimiento de Médicos'
-            }
-          },
-
-          // Editar médico
-          {
-            path: 'medico/:id',
-            component: MedicoComponent,
-            data: {
-              titulo: 'Mantenimiento de Médicos'
-            }
-          }
-    
-        ]
+        // El canload va de la mano con el lazyload ya que este valida a través del guard para ver si tiene acceso a la ruta para posteriormente cargar
+        // lo necesario y lo que se especifico en el modulo del lazyload
+        canLoad: [ AuthGuard ],
+        loadChildren: () => import( './child-routes.module' ).then( modulo => modulo.ChildRoutesModule )
       },
 ];
 

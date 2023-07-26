c5fc96bb707d937eaf8d0e9866c47b452665b060
Alquimista-07 <jdca.07@gmail.com>
2023-07-26 15:08:16 -0500

    271. Generalidades de las pruebas

---
+++ 19. Sección 22 Pruebas Unitarias/pruebas/src/app/basicas/string/string.spec.ts
@@ -0,0 +1,10 @@
+// NOTA: Todas las pruebas tienen dos parte generales, la primera es el describe() y la segunda es el it()
+//       o por lo menos con jasmine funcionan de esa forma.
+//
+//       El describe() sirve para agrupar pruebas.
+//
+//       El it() es una prueba o sirve para decir que esta es una prueba en específico,
+//       es decir, es una evaluación en concreto.
+//       Adicionalmente lo que coloquemos dentro de los paréntesis del it debe de contener
+//       un texto lo suficientemente explícito de que se trata la prueba.
+

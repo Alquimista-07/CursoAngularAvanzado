921deeb21fa3002539e3c619896dc4b2b1b9c060
Alquimista-07 <jdca.07@gmail.com>
2023-07-26 16:18:49 -0500

    272. Probando Strings

--- 19. Sección 22 Pruebas Unitarias/pruebas/src/app/basicas/string/string.spec.ts
+++ 19. Sección 22 Pruebas Unitarias/pruebas/src/app/basicas/string/string.spec.ts
@@ -8,3 +8,25 @@
 //       Adicionalmente lo que coloquemos dentro de los paréntesis del it debe de contener
 //       un texto lo suficientemente explícito de que se trata la prueba.
 
+import { mensaje } from "./string";
+
+describe( 'Pruebas de strings', () => {
+
+    it( 'Debe de regresar un string', () => {
+
+        // Acá dentro tenemos que disparar el código necesario para evaluar la función a probar,
+        // En este caso almaceno la respuesta de la función en una constante, pero puede ser en un
+        // var o un let.
+        const respuesta = mensaje('Fulanito');
+
+        // Ahora para poder evaluar y confimar la prueba, es decir, para que indicarle a jasmine y 
+        // poder confirmar si la prueba fue exitosa es necesario usar la instrucción expect(), y 
+        // este pude ser uno o varios.
+        // Adicionalmente el expect() en pocas palabras lo que indica es que espere a que la respuesta
+        // o lo que se esta evaluando sea algo en particular. Por ejemplo podemos evaluar que el tipo 
+        // que regresa la función sea obligatoriamente un string
+        expect(  typeof respuesta ).toBe('string');
+
+    });
+
+});

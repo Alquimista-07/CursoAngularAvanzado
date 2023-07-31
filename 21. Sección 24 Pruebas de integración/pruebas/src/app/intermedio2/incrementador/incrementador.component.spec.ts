20690cfcfc1a0cc1bfe6da7bc0b7ac8b8f9f73dd
Alquimista-07 <jdca.07@gmail.com>
2023-07-31 10:30:36 -0500

    303. Realizar pruebas en el HTML del componente

--- 21. Sección 24 Pruebas de integración/pruebas/src/app/intermedio2/incrementador/incrementador.component.spec.ts
+++ 21. Sección 24 Pruebas de integración/pruebas/src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { IncrementadorComponent } from './incrementador.component';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 
 describe('Incremendator Component', () => {
@@ -19,8 +20,22 @@ describe('Incremendator Component', () => {
 
     });
 
-    it('', () => {
+    it('Debe de mostrar la leyenda', () => {
 
+        component.leyenda = 'Progreso de carga';
+
+        // Disparamos la detección de cambios para indicarle a Angular que la leyenda cambió
+        fixture.detectChanges();
+
+        // Hacemos referencia al elemento html donde se muestra la leyenda
+        // NOTA: El query me permite buscar un elemento, el queryAll todos los elementos
+        //       y el queryAllNodes permite buscar todos los nodos que coincidan con el
+        //       parametro que indiquemos.
+        // NOTA2: El query nos pide como parámentro un predicado, el cual básicamente es 
+        //        una condición por la cual yo necesito buscar algo
+        const elem: HTMLElement = fixture.debugElement.query( By.css('h3') ).nativeElement;
+
+        expect( elem.innerHTML ).toContain('Progreso de carga');
 
     });
 

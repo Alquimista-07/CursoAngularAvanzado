716347e2402136e93f3c34a0d2e6fa5a82c5c503
Alquimista-07 <jdca.07@gmail.com>
2023-07-31 17:51:14 -0500

    317. Reemplazar servicios de Angular por servicios falsos

--- 22. Sección 25 Pruebas de integración intermedias y avanzadas/pruebas/src/app/avanzado/router-medico/router-medico.component.spec.ts
+++ 22. Sección 25 Pruebas de integración intermedias y avanzadas/pruebas/src/app/avanzado/router-medico/router-medico.component.spec.ts
@@ -2,13 +2,30 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RouterMedicoComponent } from './router-medico.component';
 
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, empty } from 'rxjs';
+
 describe('RouterMedicoComponent', () => {
   let component: RouterMedicoComponent;
   let fixture: ComponentFixture<RouterMedicoComponent>;
 
+  class FakeRouter{
+
+    navigate( params: any ) { }
+
+  }
+
+  class FakeActivatedRoute {
+    params: Observable<any> = empty();
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RouterMedicoComponent ]
+      declarations: [ RouterMedicoComponent ],
+      providers: [
+        { provide: Router, useClass: FakeRouter }, // Router falso: Basicamente decimos que reemplace el Router por el FakeRouter
+        { provide: ActivatedRoute, useClass: FakeActivatedRoute } // ActivatedRoute: Básicamente reemplaza el ActivatedRoute por un FakeActivatedRoute
+      ]
     })
     .compileComponents();
   });
@@ -22,4 +39,18 @@ describe('RouterMedicoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Debe de redireccionar a Medico cuando se guarde', () => {
+  
+    const router = TestBed.get( Router );
+
+    // Espiamos el router y buscar el navigate
+    const spy = spyOn( router, 'navigate' );
+
+    component.guardarMedico();
+
+    expect( spy ).toHaveBeenCalledWith( ['medico', '123'] );
+
+  });
+
 });

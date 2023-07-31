1002ae82cf615faa9d492be93b0c2b1728ae5d77
Alquimista-07 <jdca.07@gmail.com>
2023-07-31 10:20:41 -0500

    302. Material de pruebas de integración - Incrementador

--- 21. Sección 24 Pruebas de integración/pruebas/src/app/app.module.ts
+++ 21. Sección 24 Pruebas de integración/pruebas/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MedicosComponent } from './intermedio/espias/medicos.component';
 import { MedicoComponent } from './intermedio2/medico/medico.component';
 import { HospitalComponent } from './intermedio2/hospital/hospital.component';
+import { IncrementadorComponent } from './intermedio2/incrementador/incrementador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MedicosComponent,
     MedicoComponent,
-    HospitalComponent
+    HospitalComponent,
+    IncrementadorComponent
   ],
   imports: [
     BrowserModule

5e6d72272bbb825294360fd77aa9bf8a9ed7e193
Alquimista-07 <jdca.07@gmail.com>
2023-08-07 09:26:14 -0500

    324. Creación de páginas y componentes

--- 23. Sección 26 Bonus Angular, Firestore, Firebase functions, deployment y más/goty/src/app/app-routing.module.ts
+++ 23. Sección 26 Bonus Angular, Firestore, Firebase functions, deployment y más/goty/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { GotyComponent } from './pages/goty/goty.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path: 'goty',
+    component: GotyComponent
+  },
+  {
+    // Cualquier otr URL
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'inicio'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
